18bc87aee838dd3214fa0406ad4b953202ff2ad0
Huynh-Nguyen-Thanh-Huy <huyhuynh30120@gmail.com>
2022-11-12 10:49:36 +0700

    up

---
+++ catfacts/advice.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(".container__iconbox");
+const text = document.querySelector(".container__text");
+
+
+async function getAdvice() {
+
+    const res = await fetch("https://catfact.ninja/fact?max_length=64");
+    const data = await res.json();
+    text.innerText = data.fact;
+}
+
+
+button.addEventListener("click", () => getAdvice());
+
